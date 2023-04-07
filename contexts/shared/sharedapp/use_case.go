731a4439c635ea74b1fde7f8d214e35cd014bd19
Steven Bustillo <sb.sys.dev@gmail.com>
2023-04-07 09:28:25 -0600

    :construction: - added shared use case interface

---
+++ contexts/shared/sharedapp/use_case.go
@@ -0,0 +1,15 @@
+package sharedapp
+
+import "context"
+
+type CommandUseCase interface {
+	Command(interface{}) error
+}
+
+type QueryUseCase interface {
+	Query(interface{}) (interface{}, error)
+}
+
+type EventHandler interface {
+	Handle(context.Context) error
+}
