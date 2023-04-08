dece2e88737a03bfaae3f2ad58f6d48af0c8b726
Steven Bustillo <sb.sys.dev@gmail.com>
2023-04-08 09:33:33 -0600

    :construction: - updated patient entity

--- contexts/auth/authdomain/patient.go
+++ contexts/auth/authdomain/patient.go
@@ -1,16 +1,19 @@
 package authdomain
 
+import "github.com/google/uuid"
+
 // Patient Entity
 type PatientEntity struct {
+	ID       string
 	Person   Person
 	User     User
 	Contacts Contacts
-	State    string
 }
 
 // Patient Entity Factory
 func CreatePatient(person Person, user User, contacts Contacts) PatientEntity {
 	return PatientEntity{
+		ID:       uuid.New().String(),
 		Person:   person,
 		User:     user,
 		Contacts: contacts,
--- go.mod
+++ go.mod
@@ -33,6 +33,7 @@ require (
 	github.com/gin-gonic/gin v1.9.0
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/google/uuid v1.3.0
 	github.com/leodido/go-urn v1.2.2 // indirect
 	golang.org/x/crypto v0.7.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
