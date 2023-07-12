1573e8c0d552bb9dc013eb1cbfe8ecf227b45f2f
Steven Bustillo <sb.sys.dev@gmail.com>
2023-07-12 08:48:58 -0600

    :construction: - added email service simple implementation

--- contexts/auth/authinfra/patient_email_service.go
+++ contexts/auth/authinfra/patient_email_service.go
@@ -1,6 +1,15 @@
 package authinfra
 
-import "github.com/OnLab-Clinical/onlab-clinical-services/contexts/auth/authdomain"
+import (
+	"fmt"
+
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
+
+	"github.com/OnLab-Clinical/onlab-clinical-services/utils"
+
+	"github.com/OnLab-Clinical/onlab-clinical-services/contexts/auth/authdomain"
+)
 
 type PatientEmailService struct{}
 
@@ -8,5 +17,23 @@ func (service PatientEmailService) SendWelcomeEmailToPatient(patient authdomain.
 	return nil
 }
 func (service PatientEmailService) SendRecoveryEmailToPatient(patient authdomain.PatientEntity, token string) error {
+	from := mail.NewEmail("OnLab-Clinical", utils.GetEnv("SENDGRID_SENDER", ""))
+	subject := "ObLab-Clinical recovery email"
+	to := mail.NewEmail(fmt.Sprintf("%s %s", patient.Person.Name, patient.Person.Surname), string(patient.Contacts.Email))
+
+	plainTextContent := fmt.Sprintf("%s/%s", utils.GetEnv("RECOVERY_CLIENT", ""), token)
+
+	htmlContent := fmt.Sprintf(`<a href="%s/%s" target="_blank">Click Here for Recovery Password</a>`, utils.GetEnv("RECOVERY_CLIENT", ""), token)
+
+	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
+
+	client := sendgrid.NewSendClient(utils.GetEnv("SENDGRID_API_KEY", ""))
+
+	_, err := client.Send(message)
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
